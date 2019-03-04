375736bdf2b71f9dfff69b55d7f296952b14ab96
Robert Leslie <Robert.Leslie1997@gmail.com>
2019-03-03 20:09:47 -0500

    Readme Update,
    Readme updated, cleanded up some code, probably gonna Fix this with modules later when I get the chance (which seems like very tedious work, but Copy and paste is a thing)

--- consoleLightShow.rb
+++ consoleLightShow.rb
@@ -3,18 +3,6 @@
                     An Application for Joshua D. Bruce
                              By Robert Leslie 
                                    2019
-
-
-                                   TODO: 
-- In order for the TTY gem to work, my current version of ruby needs to be
- downgraded from 2.6.1 to 2.5.0, or just wait until TTY works with 2.6.1
- (Which ever can come first, I'm writing this on a train to Newark with no
- internet access so ayyy lmao)
- - Create method(s) that only prints out user specified colors
- - Get TTY to work (possible downgrading to Ruby 2.5.0?)
- - Module the hell out of this, because its getting to 200 lines of code and 
- there are certain functions that can be their own modules in a separate ruby
- file, because that looks nicer (and cooler, too!) than one VERY long .rb file.
 =end
 require 'pastel'
 
@@ -248,8 +236,6 @@ def infiniteLoopWarning(whereAmIFrom)
     end
 end
 
-titleSlide()
-
 BEGIN{
     puts "Starting ConsoleLightShow.rb..... Enjoy the show!"
 }
@@ -262,3 +248,5 @@ END{
     puts "2019"
     puts "\"Stay Lucky\""
 }
+
+titleSlide()
